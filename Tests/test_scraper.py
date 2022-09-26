8cb055cb1ef1096fddc5367d0b52ef77b65e74af
Erik Olofsson <playde5555@gmail.com>
2022-09-26 17:41:01 +0200

    La till test för pandas

--- Tests/test_scraper.py
+++ Tests/test_scraper.py
@@ -2,19 +2,27 @@ import unittest
 from parameterized import parameterized
 from App.Scraper import Scraper
 
+import pandas as pd
+
 class Test_Scraper(unittest.TestCase):
     
+    SCRAPER_SWE = Scraper("shb.a")
+    SCRAPER_US = Scraper("msft")
+
+    def test_get_ticker_of_company_no_exist(self):
+        with self.assertRaises(ValueError):
+            scraper = Scraper("noTicker")
+
     def test_get_ticker(self):
-        scraper = Scraper("msft")
-        self.assertEqual(scraper.ticker, "msft")
+        self.assertEqual(self.SCRAPER_US.ticker, "msft")
     
     def test_get_name(self):
-        scraper = Scraper("msft")
-        self.assertEqual(scraper.getName(), "Microsoft Corp.")
+        self.assertEqual(self.SCRAPER_US.getName(), "Microsoft Corp.")
 
+    # TODO
+    # change these tests (price)
     def test_get_price_american(self):
         scraper = Scraper("msft")
-        # self.assertEqual(scraper.getPrice(), 238.08)
         scraper.getPrice()
 
     def test_get_price_swedish(self):
@@ -27,19 +35,18 @@ class Test_Scraper(unittest.TestCase):
         cash_flow = scraper.getCashFlow()
         self.assertEqual(type(cash_flow), float)
 
-    def test_get_cashFlow_american(self):
-        scraper = Scraper("msft")
-        x = scraper.getCashFlow()
-        self.assertEqual(type(x), float)
-
-    def test_get_ticker_of_company_no_exist(self):
-        with self.assertRaises(ValueError):
-            scraper = Scraper("noTicker")
-
-    def test_get_pe_ratio(self):
-        scraper = Scraper("msft")
-        pe = scraper.getPriceEaringsRatio()
+    def test_get_cashFlow(self):
+        cash_flow = self.SCRAPER_SWE.getCashFlow()
+        self.assertEqual(type(cash_flow), float)
+    
+    def test_get_peratio(self):
+        pe = self.SCRAPER_SWE.getCashFlow()
         self.assertEqual(type(pe), float)
+    
+    def test_if_pandasIsofcorrectdatatype(self):
+        stock_pd = self.SCRAPER_SWE.to_pandas()
+        df = pd.DataFrame()
+        self.assertEqual(type(stock_pd), type(df))
 
 if __name__ == "__main__":
     unittest.main()
