c1077701695d8aff750b10b985e084eb8087b170
Erik Olofsson <playde5555@gmail.com>
2022-09-26 10:48:44 +0200

    La till parameterized tests

--- Tests/test_scraper.py
+++ Tests/test_scraper.py
@@ -1,4 +1,5 @@
 import unittest
+from parameterized import parameterized
 from App.Scraper import Scraper
 
 class Test_Scraper(unittest.TestCase):
@@ -20,18 +21,25 @@ class Test_Scraper(unittest.TestCase):
         scraper = Scraper("shb.a")
         scraper.getPrice()
     
-    def test_get_cashFlow_swedish(self):
-        scraper = Scraper("shb.a")
-        x = float(scraper.getCashFlow())
+    @parameterized.expand(["shb.a","msft"])
+    def test_get_cashFlow_swedish(self, ticker):
+        scraper = Scraper(ticker=ticker)
+        cash_flow = scraper.getCashFlow()
+        self.assertEqual(type(cash_flow), float)
 
     def test_get_cashFlow_american(self):
         scraper = Scraper("msft")
-        x = float(scraper.getCashFlow())
+        x = scraper.getCashFlow()
+        self.assertEqual(type(x), float)
 
     def test_get_ticker_of_company_no_exist(self):
         with self.assertRaises(ValueError):
             scraper = Scraper("noTicker")
 
+    def test_get_pe_ratio(self):
+        scraper = Scraper("msft")
+        pe = scraper.getPriceEaringsRatio()
+        self.assertEqual(type(pe), float)
 
 if __name__ == "__main__":
     unittest.main()
